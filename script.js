ef87d2c85dd85c24948a8fee75c03f5187f64fb0
Keketso <kekekeketso03@gmail.com>
2025-06-24 01:18:53 +0200

    HandlingAnEscKeypressEvent

--- script.js
+++ script.js
@@ -17,7 +17,13 @@ const showModel = function () {
 };
 for (let i = 0; i < btnShowModal.length; i++) {
   btnShowModal[i].addEventListener('click', showModel);
-
-  btnCloseModal.addEventListener('click', closeModel);
-  overlay.addEventListener('click', closeModel);
 }
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+});
